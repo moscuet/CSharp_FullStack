84b433df92c7c92ae5fd0d13a28b3f4bbaf982e7
moscuet <mos.cuet@gmail.com>
2024-05-10 14:25:59 +0300

    update user entity

--- Eshop.Core/src/Entity/User.cs
+++ Eshop.Core/src/Entity/User.cs
@@ -1,43 +1,54 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using Ecommerce.Core.src.ValueObject;
+using Ecommerce.Core.ValueObject;
 
-namespace Ecommerce.Core.src.Entity
+namespace Ecommerce.Core.Entities
 {
     [Table("users")]
     public class User : BaseEntity
     {
         [Required]
-        [MaxLength(128)]
+        [MaxLength(100)]
         public string FirstName { get; set; }
 
         [Required]
-        [MaxLength(128)]
+        [MaxLength(100)]
         public string LastName { get; set; }
 
         [Required]
-        public UserRole Role { get; set; } = UserRole.User;
+        [MaxLength(255)] 
         public string Avatar { get; set; }
 
         [Required]
-        [EmailAddress] //validation for email address that it need ot contain valid email fomat "example@example.com"
-        [MaxLength(128)]
+        [MaxLength(100)]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string PhoneNumber { get; set; }
 
         [Required]
         [MaxLength(255)]
         public string Password { get; set; }
 
-        public User(string firstName, string lastName, UserRole role, string avatar, string email, string password)
+        [Required]
+        public UserRole Role { get; set; } = UserRole.User;
+
+        // Default constructor for EF Core
+        public User() { }
+
+        // Constructor to initialize a User
+        public User(string firstName, string lastName, string avatar, string email, string phoneNumber, string password, UserRole role)
         {
             FirstName = firstName;
             LastName = lastName;
-            Role = role;
             Avatar = avatar;
             Email = email;
+            PhoneNumber = phoneNumber;
             Password = password;
+            Role = role;
         }
-
-        public User() { }
     }
 }
