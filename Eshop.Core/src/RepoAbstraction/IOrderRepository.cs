88373bd6dfd44c228fca28f0057b3e4c24de344a
moscuet <mos.cuet@gmail.com>
2024-05-13 22:36:34 +0300

    update repoOrderAbstarct

--- Eshop.Core/src/RepoAbstraction/IOrderRepository.cs
+++ Eshop.Core/src/RepoAbstraction/IOrderRepository.cs
@@ -3,12 +3,8 @@ using Eshop.Core.src.Entity;
 
 namespace Eshop.Core.src.RepositoryAbstraction
 {
-  public interface IOrderRepository
-    {
-        Task<Order> CreateOrderAsync(Order order);
-        Task<bool> UpdateOrderAsync(Order order);
-        Task<Order>? GetOrderByIdAsync(Guid orderId);
-        Task<IEnumerable<Order>> GetAllUserOrdersAsync(Guid userId, QueryOptions? options);
-        Task<bool> DeleteOrderByIdAsync(Guid orderId);
-    }
+  public interface IOrderRepository : IBaseRepository<Order>
+  {
+    Task<IEnumerable<Order>> GetAllUserOrdersAsync(Guid userId, QueryOptions? options);
   }
+}
