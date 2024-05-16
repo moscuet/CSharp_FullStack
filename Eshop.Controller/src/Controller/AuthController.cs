3b990afe7f895158a91a68322e9e089d0d355137
moscuet <mos.cuet@gmail.com>
2024-05-16 12:46:51 +0300

    Add password service

---
+++ Eshop.Controller/src/Controller/AuthController.cs
@@ -0,0 +1,24 @@
+using Eshop.Core.src.Common;
+using Eshop.Service.src.DTO;
+using Eshop.Service.src.ServiceAbstraction;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eshop.Controller.src.Controller
+{
+    [ApiController]
+    [Route("api/v1/auth")]
+    public class AuthController
+    {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public async Task<string> LoginAsync(UserCredential userCredential)
+        {
+            return await _authService.LoginAsync(userCredential);
+        }
+    }
+}
--- Eshop.WebAPI/src/Service/PasswordService.cs
+++ Eshop.WebAPI/src/Service/PasswordService.cs
@@ -5,17 +5,20 @@ namespace Eshop.WebAPI.src.Service
 {
     public class PasswordService : IPasswordService
     {
+        private const int SaltSize = 32;
+        private const int IterationCount = 20000;
+        private const int HashSize = 256 / 8;
+
         public string HashPassword(string password, out byte[] salt)
         {
-            salt = RandomNumberGenerator.GetBytes(16);
+            salt = RandomNumberGenerator.GetBytes(SaltSize);
             string hashed = Convert.ToBase64String(
-                KeyDerivation.Pbkdf2
-                (
-                        password: password!,
-                        salt: salt,
-                        prf: KeyDerivationPrf.HMACSHA256,
-                        iterationCount: 600000,
-                        numBytesRequested: 256 / 8
+                KeyDerivation.Pbkdf2(
+                    password: password,
+                    salt: salt,
+                    prf: KeyDerivationPrf.HMACSHA256,
+                    iterationCount: IterationCount,
+                    numBytesRequested: HashSize
                 )
             );
             return hashed;
@@ -24,16 +27,15 @@ namespace Eshop.WebAPI.src.Service
         public bool VerifyPassword(string password, string passwordHash, byte[] salt)
         {
             string hashed = Convert.ToBase64String(
-                 KeyDerivation.Pbkdf2
-                 (
-                         password: password!,
-                         salt: salt,
-                         prf: KeyDerivationPrf.HMACSHA256,
-                         iterationCount: 600000,
-                         numBytesRequested: 256 / 8
+                 KeyDerivation.Pbkdf2(
+                     password: password,
+                     salt: salt,
+                     prf: KeyDerivationPrf.HMACSHA256,
+                     iterationCount: IterationCount,
+                     numBytesRequested: HashSize
                  )
              );
-             return hashed == passwordHash;
+            return hashed == passwordHash;
         }
     }
 }
