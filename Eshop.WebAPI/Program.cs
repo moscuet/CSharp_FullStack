dbcedef029d5dfe5d720dd78e7d4bd6fba572c22
moscuet <mos.cuet@gmail.com>
2024-05-16 12:48:40 +0300

    modify db context to implement  authorization

--- Eshop.WebAPI/Program.cs
+++ Eshop.WebAPI/Program.cs
@@ -1,4 +1,9 @@
+using System.Text;
 using System.Text.Json.Serialization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Npgsql;
 using Eshop.Core.src.RepoAbstraction;
 using Eshop.Core.src.ValueObject;
 using Eshop.Service.src.Service;
@@ -6,9 +11,8 @@ using Eshop.Service.src.ServiceAbstraction;
 using Eshop.WebApi.src.Data;
 using Eshop.WebApi.src.middleware;
 using Eshop.WebApi.src.Repo;
+using Eshop.WebApi.src.Service;
 using Eshop.WebAPI.src.Service;
-using Microsoft.EntityFrameworkCore;
-using Npgsql;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -35,14 +39,33 @@ builder.Services.AddDbContext<EshopDbContext>
     // .AddInterceptors(new TimeStampInteceptor())
 );
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+.AddJwtBearer(
+    options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Secrets:JwtKey"])),
+            ValidateIssuer = true,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Secrets:Issuer"]
+        };
+    }
+);
+
+
 // service registration -> automatically create all instances of dependencies
 builder.Services.AddScoped<IUserRepository, UserRepo>();
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IPasswordService, PasswordService>();
+builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
 
 builder.Services.AddScoped<ExceptionHandlerMiddleware>();
-
 builder.Services.AddAutoMapper(typeof(MappingProfile).Assembly);
+
 var app = builder.Build();
 
 app.UseSwagger();
@@ -50,85 +73,11 @@ app.UseSwaggerUI();
 
 app.UseMiddleware<ExceptionHandlerMiddleware>();
 app.UseHttpsRedirection();
+
+// Add authentication and authorization middleware
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
-
-
-
-
-//Add all controllers
-
-// builder.Services.AddDbContext<EshopDbContext>(options =>
-// {
-//     options.UseNpgsql(builder.Configuration.GetConnectionString("PgDbConnection"));
-// });
-
-
-
-
-// service registration -> automatically create all instances of dependencies
-///builder.Services.AddScoped<IUserRepository, UserRepo>();
-//builder.Services.AddScoped<IUserService, UserService>();
-// builder.Services.AddScoped<IUserService, UserService>();
-// builder.Services.AddScoped<ITokenService, TokenService>();
-// builder.Services.AddScoped<IAuthService, AuthService>();
-//builder.Services.AddScoped<ExceptionHandlerMiddleware>();
-// builder.Services.AddScoped<ICategoryRepository,CategoryRepo>();
-// builder.Services.AddScoped<ICategoryService,CategoryService>();
-// builder.Services.AddScoped<IProductRepository,ProductRepo>();
-// builder.Services.AddScoped<IProductService,ProductService>();
-// builder.Services.AddScoped<IOrderRepository,OrderRepo>(); // need to implement address repo
-// builder.Services.AddScoped<IOrderService,OrderService>();
-// builder.Services.AddScoped<IReviewRepository,ReviewRepo>();
-// builder.Services.AddScoped<IReviewService,ReviewService>();
-
-
-// regist authroization handler (alia)
-// builder.Services.AddSingleton<IAuthorizationHandler, VerifyResourceOwnerHandler>();
-
-
-// Add authentication instructions
-// builder
-//     .Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-//     .AddJwtBearer(options =>
-//     {
-//         options.TokenValidationParameters = new TokenValidationParameters
-//         {
-//             IssuerSigningKey = new SymmetricSecurityKey(
-//                 Encoding.UTF8.GetBytes(builder.Configuration["Secrets:JwtKey"])
-//             ),
-//             ValidateIssuer = true,
-//             ValidateAudience = false,
-//             ValidateLifetime = true,
-//             ValidateIssuerSigningKey = true,
-//             ValidIssuer = builder.Configuration["Secrets:Issuer"]
-//         };
-//     });
-
-// Add authentication instructions (alia)
-
-
-// builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-// .AddJwtBearer(
-//     options =>
-//     {
-//         options.TokenValidationParameters = new TokenValidationParameters
-//         {
-//             IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Secrets:JwtKey"])),
-//             ValidateIssuer = true,
-//             ValidateAudience = false,
-//             ValidateLifetime = true, // amke sure it's not expired
-//             ValidateIssuerSigningKey = true,
-//             ValidIssuer = builder.Configuration["Secrets:Issuer"]
-//         };
-//     }
-// );
-
-// builder.Services.AddAuthorization(
-//     policy => 
-//     {
-//         policy.AddPolicy("ResourceOwner", policy => policy.Requirements.Add(new VerifyResourceOwnerRequirement()));
-//         policy.AddPolicy("GoldenMemberOnly", policy => policy.RequireClaim("Membership", "Golden"));
-//     }
-// );
--- Eshop.WebAPI/src/Data/EshopDbContext.cs
+++ Eshop.WebAPI/src/Data/EshopDbContext.cs
@@ -4,14 +4,12 @@ using Eshop.Core.src.Entity;
 using Eshop.Core.src.ValueObject;
 // using Eshop.WebApi.src.Data.Interceptors;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace Eshop.WebApi.src.Data
 {
     public class EshopDbContext : DbContext
     {
         protected readonly IConfiguration _config;
-    
 
         public DbSet<User>? Users { get; set; }
 
@@ -28,74 +26,28 @@ namespace Eshop.WebApi.src.Data
             _loggerFactory = loggerFactory;
         }
 
-
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder
             .UseSnakeCaseNamingConvention();
         }
 
-   
-
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.HasPostgresExtension("uuid-ossp");
             // craete enum table
             modelBuilder.HasPostgresEnum<UserRole>();
             modelBuilder.HasPostgresEnum<OrderStatus>();
             modelBuilder.HasPostgresEnum<SortBy>();
             modelBuilder.HasPostgresEnum<SortOrder>();
 
-            // add constrain for database between tables as we cant do it using notation
-            modelBuilder.HasPostgresExtension("uuid-ossp");
-
             modelBuilder.Entity<User>(entity =>
-             {
-                 entity.Property(e => e.FirstName).IsRequired().HasMaxLength(50);
-                 entity.Property(e => e.LastName).IsRequired().HasMaxLength(50);
-                 entity.Property(e => e.Email).IsRequired().HasMaxLength(100).HasColumnType("varchar");
-                 entity.Property(e => e.Password).IsRequired().HasMaxLength(255);
-                //  entity.Property(e => e.Salt).IsRequired();
-                 entity.Property(e => e.PhoneNumber).IsRequired().HasMaxLength(20);
-                 entity.Property(e => e.Avatar).HasMaxLength(2048).HasDefaultValue(AppConstants.AVATAR_DEFAULT_IMAGE);
-        
-             });
+                {
+                    entity.Property(e => e.UserRole)
+                              .HasConversion<string>();
+                    entity.HasIndex(e => e.Email).IsUnique();
+                    entity.HasIndex(e => e.UserRole);
+                });
         }
     }
 }
-
-
-
-
-    // public DbSet<Category> Categories { get; set; }
-        // public DbSet<Product> Products { get; set; }
-        // public DbSet<Order> Orders { get; set; }
-        // public DbSet<Review> Reviews { get; set; }
-
-
-
-        //      protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        // {
-        //     optionsBuilder
-        //     // .AddInterceptors(new TimestampInterceptor())
-        //     // .EnableSensitiveDataLogging()
-        //     // .EnableDetailedErrors()
-        //     .UseSnakeCaseNamingConvention();
-        // }
-
-
-
-       /// OleDbPermission mine: 
-             // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        // {
-        //     optionsBuilder.UseNpgsql(configuration.GetConnectionString("PgDbConnection")).UseSnakeCaseNamingConvention();
-        //     // optionsBuilder.UseNpgsql(configuration.GetConnectionString("PgDbConnection")).UseSnakeCaseNamingConvention().AddInterceptors(new SqlLoggingInterceptor(_loggerFactory.CreateLogger<SqlLoggingInterceptor>()));
-
-        // }
-
-
-                //  entity.Property(e => e.Role)
-                //              .IsRequired()
-                //              .HasDefaultValue(UserRole.User)
-                //              .HasConversion<string>()
-                //              .Metadata.SetAfterSaveBehavior(PropertySaveBehavior.Save);
