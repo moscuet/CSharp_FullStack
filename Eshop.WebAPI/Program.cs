fef6faaf4cbf54331c58bba79aad0fc20f3ceb7a
moscuet <mos.cuet@gmail.com>
2024-05-24 15:16:10 +0300

    fixed connnection issues

--- Eshop.WebAPI/Program.cs
+++ Eshop.WebAPI/Program.cs
@@ -1,3 +1,4 @@
+
 using System.Text;
 using System.Text.Json.Serialization;
 using Microsoft.EntityFrameworkCore;
@@ -16,25 +17,27 @@ using DotNetEnv;
 using Npgsql;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var localConnectionString = "Host=localhost;Port=5432;Database=eshop;Username=test_admin;Password=testadminsecret;";
+
  Env.Load();
- var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL") ?? throw new InvalidOperationException("Database connection string 'DATABASE_URL' not found.");
+// var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL") ?? throw new InvalidOperationException("Database connection string 'DATABASE_URL' not found.");
  var jwtKey = Environment.GetEnvironmentVariable("JWT_KEY") ?? throw new InvalidOperationException("JWT Key is not set.");
 var issuer = Environment.GetEnvironmentVariable("JWT_ISSUER") ?? throw new InvalidOperationException("JWT Issuer is not set.");
+// var Port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+// var Host = Environment.GetEnvironmentVariable("HOST") ?? "0.0.0.0";
 
-var Port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-var Host = Environment.GetEnvironmentVariable("HOST") ?? "0.0.0.0";
-
-// Parse the DATABASE_URL
-var databaseUri = new Uri(databaseUrl);
-var userInfo = databaseUri.UserInfo.Split(':');
-var connectionString = new NpgsqlConnectionStringBuilder
-{
-    Host = databaseUri.Host,
-    Port = databaseUri.Port,
-    Username = userInfo[0],
-    Password = userInfo[1],
-    Database = databaseUri.LocalPath.TrimStart('/')
-}.ToString();
+// // Parse the DATABASE_URL
+// var databaseUri = new Uri(databaseUrl);
+// var userInfo = databaseUri.UserInfo.Split(':');
+// var connectionString = new NpgsqlConnectionStringBuilder
+// {
+//     Host = databaseUri.Host,
+//     Port = databaseUri.Port,
+//     Username = userInfo[0],
+//     Password = userInfo[1],
+//     Database = databaseUri.LocalPath.TrimStart('/')
+// }.ToString();
 
 // Configure services...
 builder.Services.AddEndpointsApiExplorer();
@@ -45,9 +48,8 @@ builder.Services.AddControllers().AddJsonOptions(options =>
     options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
 });
 
-
 builder.Services.AddDbContext<EshopDbContext>(
-    options => options.UseNpgsql(connectionString)
+    options => options.UseNpgsql(localConnectionString)
                       .UseSnakeCaseNamingConvention()
                       .AddInterceptors(new TimeStampInterceptor()));
 
@@ -110,29 +112,37 @@ using (var scope = app.Services.CreateScope())
 {
     var dbContext = scope.ServiceProvider.GetRequiredService<EshopDbContext>();
     dbContext.Database.Migrate();
-    await dbContext.SeedDataAsync();
 }
 
-// Configure middlewares...
-app.UseSwagger();
-app.UseSwaggerUI(c =>
-{
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Eshop API V1");
-    c.RoutePrefix = string.Empty;
-});
+
+// // Configure middlewares...
+// app.UseSwagger();
+// app.UseSwaggerUI(c =>
+// {
+//     c.SwaggerEndpoint("/swagger/v1/swagger.json", "Eshop API V1");
+//     c.RoutePrefix = string.Empty;
+// });
 
 app.UseSwagger();
-app.UseSwaggerUI(c =>
-{
-   c.SwaggerEndpoint("/swagger/v1/swagger.json", "Eshop API V1");
-   c.RoutePrefix = string.Empty;
-});
-
+app.UseSwaggerUI();
 
 app.UseMiddleware<ExceptionHandlerMiddleware>();
 app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
-// app.Run();
-app.Run($"http://0.0.0.0:{Port}");
+app.Run();
+// app.Run($"http://0.0.0.0:{Port}");
+
+
+//     dbContext.Database.Migrate();
+// //app.Run($"http://0.0.0.0:{Port}");
+//     await dbContext.SeedDataAsync();
+
+    
+// app.UseSwagger();
+// app.UseSwaggerUI(c =>
+// {
+//     c.SwaggerEndpoint("/swagger/v1/swagger.json", "Eshop API V1");
+//     c.RoutePrefix = string.Empty;
+// });
